fd19c484f0ea09b6732fa78416aee12895658e4d
vizenxx <vizenxjp@gmail.com>
2026-02-09 13:27:20 +0600

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  base: '/portfolio-site/',
-  server: {
-    host: true,
-    port: 5173,
-  }
-})
+});
+
